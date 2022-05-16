0481c943ab74fee66d780c14c3cdd93c9ab8a5b1
Verlygo Dmitry <56788178+ToxicButterfly@users.noreply.github.com>
2022-05-16 17:21:39 +0300

    Dao

--- src/main/java/com/teamA/spring/rest/dao/EmployeeDAO.java
+++ src/main/java/com/teamA/spring/rest/dao/EmployeeDAO.java
@@ -1,4 +1,13 @@
 package com.teamA.spring.rest.dao;
 
+import com.teamA.spring.rest.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
 }
