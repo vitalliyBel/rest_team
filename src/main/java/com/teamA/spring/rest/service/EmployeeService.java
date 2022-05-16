e096c0cb377061146be10c63aeec49188aaa0680
Verlygo Dmitry <56788178+ToxicButterfly@users.noreply.github.com>
2022-05-16 17:45:07 +0300

    Service

--- src/main/java/com/teamA/spring/rest/service/EmployeeService.java
+++ src/main/java/com/teamA/spring/rest/service/EmployeeService.java
@@ -1,4 +1,15 @@
 package com.teamA.spring.rest.service;
 
+import com.teamA.spring.rest.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
 }
