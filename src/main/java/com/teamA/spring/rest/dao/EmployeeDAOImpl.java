db3dbf9701f5058f8a5f99382f8983208e80658c
EpifanovTolik <zuradik1000@yandex.ru>
2022-05-17 12:54:13 +0300

    Добавил @Repository в EmpDaoImpl

--- src/main/java/com/teamA/spring/rest/dao/EmployeeDAOImpl.java
+++ src/main/java/com/teamA/spring/rest/dao/EmployeeDAOImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Autowired
