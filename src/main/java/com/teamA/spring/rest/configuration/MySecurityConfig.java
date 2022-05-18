a091b88a86783722090167d7483fef4df12f5803
EpifanovTolik <zuradik1000@yandex.ru>
2022-05-18 08:55:20 +0300

    Добавил secure

---
+++ src/main/java/com/teamA/spring/rest/configuration/MySecurityConfig.java
@@ -0,0 +1,53 @@
+package com.teamA.spring.rest.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+//        auth.jdbcAuthentication().dataSource(dataSource); //тут смотрит уже из базы данных инфу
+        // храним пароли прям тут(но лучше в базе)
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder
+                        .username("anatoli")
+                         .password("anatoli")
+                        .roles("EMPLOYEE"))
+                .withUser(userBuilder
+                        .username("vitaliy")
+                        .password("vitaliy")
+                        .roles("HR"))
+                .withUser(userBuilder
+                        .username("nikita")
+                        .password("nikita")
+                        .roles("MANAGER", "HR"))
+                .withUser(userBuilder
+                        .username("dmitry")
+                        .password("dmitry")
+                        .roles("EMPLOYEE"))
+                 .withUser(userBuilder
+                    .username("aleksandr")
+                    .password("aleksandr")
+                    .roles("EMPLOYEE"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/employees").hasAnyRole("EMPLOYEE", "HR", "MANAGER") //всем доступна страница
+
+                .and().formLogin().permitAll(); //логин и пароль будет запрашиватья у всех
+    }
+}
---
+++ src/main/java/com/teamA/spring/rest/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.teamA.spring.rest.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
